8447ae2f3608e109fd050faf5f5af663a85dbc9d
Aditya Banka <adityabanka1947@gmail.com>
2025-03-25 05:49:42 +0530

    First steps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">hmm ok this is fast!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
